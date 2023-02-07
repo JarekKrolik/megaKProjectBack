d6ce406c0b3beeb31f89bd3697b35c5423d7ffbc
JarekKrolik <madmax@onet.eu>
2023-02-07 20:21:16 +0100

    Added rate limiter.

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import express, {json, Request, Response} from "express";
 import cors from 'cors';
 import 'express-async-errors';
 import {handleErrors, ValidationError} from "./utils/handleErrors";
-
+import rateLimit from 'express-rate-limit';
 
 
 const app =express()
@@ -12,9 +12,11 @@ app.use(cors({
     origin:'http://localhost:3000',
 }));
 app.use(json());
-app.get('/',async (res:Response,req:Request)=>{
-    throw new ValidationError('daaamn!!!')
-})
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+}))
+
 app.use(handleErrors)
 app.listen(3001,'0.0.0.0',()=>{
     console.log('listening on port http://localhost:3001')
