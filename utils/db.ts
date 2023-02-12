fc16ea5f469183b6a340248c16ea446c454ada5b
JarekKrolik <madmax@onet.eu>
2023-02-12 21:39:19 +0100

    changed database config

--- utils/db.ts
+++ utils/db.ts
@@ -2,8 +2,9 @@ import {createPool} from "mysql2/promise";
 
 export const pool =createPool({
     host:'localhost',
-    user:'root',
-    database:'megak_mapproject',
+    user:'madmaxla_api',
+    password:'loF504jlnj',
+    database:'madmaxla_api',
     namedPlaceholders:true,
     decimalNumbers:true,
 })
